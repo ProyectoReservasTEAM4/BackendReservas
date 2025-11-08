1a4478befd3dc7ed291d7c9b81a8fdf50bceaac8
rogerbetan201@gmail.com <Rolleiro-betancur>
2025-11-07 21:44:58 -0500

    Capa de persistence

--- src/main/java/com/Spring/ProyectoJpaReservas/persistence/entity/Cliente.java
+++ src/main/java/com/Spring/ProyectoJpaReservas/persistence/entity/Cliente.java
@@ -1,4 +1,24 @@
 package com.Spring.ProyectoJpaReservas.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Nombre")
+    private String name;
+
+
 }
