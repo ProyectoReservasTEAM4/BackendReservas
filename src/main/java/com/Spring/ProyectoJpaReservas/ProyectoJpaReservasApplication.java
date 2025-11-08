c7868ef7c27c88f61440ffc6585b8d5ff2a33b09
rogerbetan201@gmail.com <Rolleiro-betancur>
2025-11-07 20:41:52 -0500

    Creando capas

---
+++ src/main/java/com/Spring/ProyectoJpaReservas/ProyectoJpaReservasApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.ProyectoJpaReservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJpaReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJpaReservasApplication.class, args);
+	}
+
+}
